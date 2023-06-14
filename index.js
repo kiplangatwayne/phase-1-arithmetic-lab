aa432543588c118b3fb2d0a965c0ea92a111777a
kiplangatwayne <waynekiplangatkorir903@gmail.com>
2023-06-14 08:43:11 +0300

    completed Assinment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+console.log(multiply)
+
+let random = Math.floor(Math.random() * 5) + 1;
+
+let num3 = 18;
+let num4 = 7;
+
+let mod = num3 % num4;
+
+let max = Math.max(5, 9, 11, 12, 14, 16, 20);
